06a3b63afa1c2fd6ad00ae96f4b6d4301c1f118c
Naiim Hassouna <naiimhassouna0@gmail.com>
2024-06-25 15:11:11 +0300

    expo 51 + fixed prop types

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,65 +1,51 @@
+import React from "react";
 import { View, ScrollView } from "react-native";
-import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useColorScheme } from "nativewind";
-import { StatusBar } from "expo-status-bar";
+import { useQuery } from "@tanstack/react-query";
+import { fetchBreakingNews, fetchRecommendedNews } from "../../utils/NewsApi";
 import Loading from "../components/Loading/Loading";
 import Header from "../components/Header/Header";
 import NewsSection from "../components/NewsSection/NewsSection";
-import { useQuery } from "@tanstack/react-query";
-import { fetchBreakingNews, fetchRecommendedNews } from "../../utils/NewsApi";
 import MiniHeader from "../components/Header/MiniHeader";
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import BreakingNews from "../components/BreakingNews";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 export default function HomeScreen() {
-  const { colorScheme, toggleColorScheme } = useColorScheme();
-  
-  // Breaking News
-  const { data, isLoading: isBreakingLoading } = useQuery({
-    queryKey: ["breakingNewss"],
+  const { data: breakingNewsData, isLoading: isBreakingLoading } = useQuery({
+    queryKey: ["breakingNews"],
     queryFn: fetchBreakingNews,
   });
 
-  // Recommended News
-  const { data: recommendedNew, isLoading: isRecommendedLoading } = useQuery({
-    queryKey: ["recommededNewss"],
-    queryFn: fetchRecommendedNews,
-  });
+  const { data: recommendedNewsData, isLoading: isRecommendedLoading } =
+    useQuery({
+      queryKey: ["recommendedNews"],
+      queryFn: fetchRecommendedNews,
+    });
 
   return (
-    <SafeAreaView className=" flex-1 bg-white dark:bg-neutral-900">
-      <StatusBar style={colorScheme == "dark" ? "light" : "dark"} />
-
-      <View>
-        {/* Header */}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{ flex: 1 }}>
         <Header />
 
-        {/* Breaking News */}
         {isBreakingLoading ? (
           <Loading />
         ) : (
-          <View className="">
+          <View style={{ marginBottom: hp(3) }}>
             <MiniHeader label="Breaking News" />
-            <BreakingNews label="Breaking News" data={data.articles} />
+            <BreakingNews data={breakingNewsData.articles} />
           </View>
         )}
 
-        {/* Recommended News */}
         <View>
           <MiniHeader label="Recommended" />
           <ScrollView
-            contentContainerStyle={{
-              paddingBottom: hp(80),
-            }}
+            contentContainerStyle={{ paddingBottom: hp(8) }}
+            showsVerticalScrollIndicator={false}
           >
             {isRecommendedLoading ? (
               <Loading />
             ) : (
-              <NewsSection
-                label="Recommendation"
-                newsProps={recommendedNew.articles}
-              />
+              <NewsSection newsProps={recommendedNewsData.articles} />
             )}
           </ScrollView>
         </View>
